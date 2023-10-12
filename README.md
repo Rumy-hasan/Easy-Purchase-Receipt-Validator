1ed6f1e0931681e9479288e9b0a21514e53dd2f8
rndbjitgroup <91940733+rndbjitgroup@users.noreply.github.com>
2023-10-12 09:36:14 +0600

    Create project - It’s an initial commit for setup. - Have demo project and our package. Ref: https://redmine.bjitgroup.com/redmine/issues/483311

---
+++ README.md
@@ -0,0 +1,2 @@
+# Easy-Purchase-Receipt-Validator
+In App Purchase Receipt Validator
---
+++ Tests/Easy-Purchase-Receipt-ValidatorTests/Easy_Purchase_Receipt_ValidatorTests.swift
@@ -0,0 +1,12 @@
+import XCTest
+@testable import Easy_Purchase_Receipt_Validator
+
+final class Easy_Purchase_Receipt_ValidatorTests: XCTestCase {
+    func testExample() throws {
+        // XCTest Documenation
+        // https://developer.apple.com/documentation/xctest
+
+        // Defining Test Cases and Test Methods
+        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    }
+}
